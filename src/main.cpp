61d28b225024a808aa1a175b4fd84f81a9de5740
ThIsJaCk <y23657689@gmail.com>
2022-06-15 03:37:22 +0800

    Add a sphere

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,20 @@
 #include "Ray.hpp"
 #include "Image/BMP.hpp"
 
+bool IsHitSphere(const point3& center, const double& radius, const Ray& r) {
+    vec3 oc = r.m_origin - center;
+    auto a = dot(r.m_direction, r.m_direction);
+    auto b = 2.0 * dot(oc, r.m_direction);
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = b * b - 4 * a * c;
+    return (discriminant > 0);
+}
+
 color RayColor(const Ray& r) {
+    if (IsHitSphere(point3(0, 0, -1), 0.5, r)) {
+        return color(1, 0, 0);
+    }
+
     vec3 unit_dir = normalize(r.m_direction);
     auto t = 0.5 * (unit_dir.y + 1.0);
     return lerp({1.0, 1.0, 1.0}, {0.5, 0.7, 1.0}, t);
